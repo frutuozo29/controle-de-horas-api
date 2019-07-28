94e44e7da71f7ddf49920a4c91ed56499a0ed2c1
Renan Frutuozo <frutuozo29@gmail.com>
2019-07-28 10:46:12 -0300

    validando o authorization

--- src/jwt/index.js
+++ src/jwt/index.js
@@ -3,15 +3,19 @@ const key = require('./key')
 
 module.exports.configureJwt = server => {
   server.use((req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1]
+    try {
+      const token = req.headers.authorization && req.headers.authorization.split(' ')[1]
 
-    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' })
+      if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' })
 
-    jwt.verify(token, key.tokenKey, async (error, payload) => {
-      if (error) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' })
+      jwt.verify(token, key.tokenKey, async (error, payload) => {
+        if (error) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' })
 
-      req.user = payload
-      next()
-    })
+        req.user = payload
+        next()
+      })
+    } catch (error) {
+      return res.status(500).send({ auth: false, message: error })
+    }
   })
 }
